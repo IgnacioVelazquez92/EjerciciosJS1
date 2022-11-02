5b0e1775a2fdcf33d4eebcb23be702612a677cdb
Ignacio Velazquez <ignaciov92@gmail.com>
2022-11-01 22:30:35 -0300

    ultimo arreglo ej 11

--- ejercicio11.js
+++ ejercicio11.js
@@ -1,12 +1,13 @@
 // Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 y que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
 
 let num =parseInt(prompt("escribe un numero por favor:"))
-let contador=0
 let div=""
 for (let i =1 ; i<8 ; i++) {
   if (num%i==0)
     if(i==2 || i==3 || i==5 || i==7){
       div= div + String(i); 
+    }else{
+      document.write(`${num} No es divisible por los números 2, 3 , 5 ni 7`)
     }
 }
 for(let i=0;i<div.length;i++){
