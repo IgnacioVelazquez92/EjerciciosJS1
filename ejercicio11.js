f9174353a96257c400dd5a9273b55ccd64007bb4
Ignacio Velazquez <ignaciov92@gmail.com>
2022-11-01 22:37:28 -0300

    arreglo final

--- ejercicio11.js
+++ ejercicio11.js
@@ -6,10 +6,12 @@ for (let i =1 ; i<8 ; i++) {
   if (num%i==0)
     if(i==2 || i==3 || i==5 || i==7){
       div= div + String(i); 
-    }else{
-      document.write(`${num} No es divisible por los números 2, 3 , 5 ni 7`)
     }
 }
-for(let i=0;i<div.length;i++){
-  document.write(`es divisible por ${div[i]}<br>`)
+if (div!="") {
+  for(let i=0;i<div.length;i++){
+    document.write(`es divisible por ${div[i]}<br>`)
+  }
+}else{
+  document.write(`${num} No es divisible por los números 2, 3 , 5 ni 7`)
 }
